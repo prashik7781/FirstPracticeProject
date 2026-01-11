7433340f86a2280db6c6e146569393eda5be1c00
prashik7781 <prashikingle200@gmail.com>
2026-01-11 09:58:08 +0530

    first commit of project

---
+++ MFirstGitProject/src/com/soft/GitPractice.java
@@ -0,0 +1,8 @@
+package com.soft;
+
+public class GitPractice {
+	public static void main(String[] args) {
+		System.out.println("kalshetty aappa");
+		System.out.println("Welcome to Wakad");
+	}
+}
