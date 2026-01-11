9faf78b2fda46fc212f5a4e43a8f84e300832d18
prashik7781 <prashikingle200@gmail.com>
2026-01-11 10:06:43 +0530

    added one new line

--- MFirstGitProject/src/com/soft/GitPractice.java
+++ MFirstGitProject/src/com/soft/GitPractice.java
@@ -4,5 +4,6 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("kalshetty aappa");
 		System.out.println("Welcome to Wakad");
+		System.out.println("are plan unsuccessful due to lecture");
 	}
 }
