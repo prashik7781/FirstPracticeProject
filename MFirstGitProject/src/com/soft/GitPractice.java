7c20800253ca8bb6f1bb55c8747bb6c5861bdf67
prashik7781 <prashikingle200@gmail.com>
2026-01-11 10:25:54 +0530

    new changes

--- MFirstGitProject/src/com/soft/GitPractice.java
+++ MFirstGitProject/src/com/soft/GitPractice.java
@@ -2,9 +2,7 @@ package com.soft;
 
 public class GitPractice {
 	public static void main(String[] args) {
-		System.out.println("kalshetty aappa");
-		System.out.println("Welcome to Wakad");
-		System.out.println("are plan unsuccessful due to lecture");
-		System.out.println("but i will go");
+		System.out.println("Welcome to Java..");
+		System.out.println("Welcome to Git ");
 	}
 }
