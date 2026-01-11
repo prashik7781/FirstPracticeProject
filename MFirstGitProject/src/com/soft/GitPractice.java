85529b2f8489c0f19a599cf01243ada978983ab2
Prashik Ingle <prashikingle200@gmail.com>
2026-01-11 10:12:40 +0530

    third commit

--- MFirstGitProject/src/com/soft/GitPractice.java
+++ MFirstGitProject/src/com/soft/GitPractice.java
@@ -5,5 +5,6 @@ public class GitPractice {
 		System.out.println("kalshetty aappa");
 		System.out.println("Welcome to Wakad");
 		System.out.println("are plan unsuccessful due to lecture");
+		System.out.println("but i will go");
 	}
 }
